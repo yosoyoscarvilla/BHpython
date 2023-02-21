96b7ed9b147fbb5cc8a6effcfe7507006bf8fb1d
yosoyoscarvilla <yosoyoscarvilla@gmail.com>
2023-02-20 22:53:11 -0600

    ssh fully complete. Details on format (b appearing)

--- SSh/rcmd.py
+++ SSh/rcmd.py
@@ -3,13 +3,13 @@ import shlex
 import subprocess
 
 def command(ip, port, user, passwd, cmd):
-    client = paramiko.SSHCLient()
+    client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(ip, username=user, password=passwd)
+    client.connect(ip, port=port, username=user, password=passwd)
     ssh_session = client.get_transport().open_session()
 
     if ssh_session.active:
-        ssh_session.send(command)
+        ssh_session.send(cmd)
         print(ssh_session.recv(1024))
         while True:
             command = ssh_session.recv(1024)
@@ -25,12 +25,8 @@ def main():
     import getpass
     #getpass puede recuperar el user de la sesion actual
     #oculata el input del password
-    user = input('Username: ')
-    password = getpass.getpass()
-
-    ip = input('Enter server IP: ')
-    port = input('Enter port: ')
-    cmd = input('Enter command: ')
-    command(ip, port,user, password, cmd)
+    #ip = input('Enter server IP: ')
+    #port = input('Enter port: ')
+    command('10.0.2.15', 2222,'root', 'toor', 'ClientConnected')
 
 main()
